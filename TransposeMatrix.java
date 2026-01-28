4d8313068ed53054dd832079cd568013b40c8284
Aylin M <aylin.ma24@gmail.com>
2026-01-28 14:42:48 +0100

    Week 3 Activities

---
+++ TransposeMatrix.java
@@ -0,0 +1,19 @@
+public class TransposeMatrix {
+    public static void main(String[] args) {
+        int[][] A = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        int rows = A.length;
+        int cols = A[0].length;
+        int[][] T = new int[cols][rows];
+
+        // Transpose logic
+        //TODO
+
+        // Print transposed matrix
+        System.out.println("Transposed Matrix:");
+        //TODO
+    }
+}
